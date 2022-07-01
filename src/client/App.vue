49bb3d66018b8cb6df225ec56070bb49407a8a9f
yanghong <15034009501@163.com>
2022-07-01 15:43:54 +0800

    忽略

--- src/client/App.vue
+++ src/client/App.vue
@@ -1,11 +1,4 @@
 <template>
-  <y-button ref="ref1">click</y-button>
+  <y-button ref="button">click</y-button>
 </template>
-<script setup lang="ts">
-import {ref,onMounted} from 'vue'
-const ref1 = ref<DemoButtonFunc>()
-onMounted(() => {
-  // ref1.value?.setProperty('')
-})
-
-</script>
+<script setup lang="ts"></script>
